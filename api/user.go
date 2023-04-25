985c310e8b2a2c63f28611248468dd7c7833b8ae
Neidn <dty3152@gmail.com>
2023-04-25 16:09:32 +0900

    change response after creating user

--- api/user.go
+++ api/user.go
@@ -6,13 +6,22 @@ import (
 	"net/http"
 	db "practice-docker/db/sqlc"
 	"practice-docker/util"
+	"time"
 )
 
 type createUserRequest struct {
-	Username string `json:"username" binding:"required, alphanum"`
-	Password string `json:"password" binding:"required, min=6"`
+	Username string `json:"username" binding:"required,alphanum"`
+	Password string `json:"password" binding:"required,min=6"`
 	FullName string `json:"full_name" binding:"required"`
-	Email    string `json:"email" binding:"required, email"`
+	Email    string `json:"email" binding:"required,email"`
+}
+
+type createUserResponse struct {
+	Username          string    `json:"username"`
+	FullName          string    `json:"full_name"`
+	Email             string    `json:"email"`
+	PasswordChangedAt time.Time `json:"password_changed_at"`
+	CreatedAt         time.Time `json:"created_at"`
 }
 
 // POST /users
@@ -55,5 +64,12 @@ func (server *Server) createUser(ctx *gin.Context) {
 	}
 
 	// Return the user.
-	ctx.JSON(http.StatusOK, user)
+	rsp := createUserResponse{
+		Username:          user.Username,
+		FullName:          user.FullName,
+		Email:             user.Email,
+		PasswordChangedAt: user.PasswordChangedAt,
+		CreatedAt:         user.CreatedAt,
+	}
+	ctx.JSON(http.StatusOK, rsp)
 }

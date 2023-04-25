dadab4cc614405b6aa5fe741c2e1700e7ab53e3e
Neidn <dty3152@gmail.com>
2023-04-25 15:50:14 +0900

    test creating password and user

--- util/password_test.go
+++ util/password_test.go
@@ -9,14 +9,19 @@ import (
 func TestHashPassword(t *testing.T) {
 	password := RandomString(10)
 
-	hashedPassword, err := HashPassword(password)
+	hashedPassword1, err := HashPassword(password)
 	require.NoError(t, err)
-	require.NotEmpty(t, hashedPassword)
+	require.NotEmpty(t, hashedPassword1)
 
-	err = CheckPassword(password, hashedPassword)
+	err = CheckPassword(password, hashedPassword1)
 	require.NoError(t, err)
 
 	wrongPassword := RandomString(10)
-	err = CheckPassword(wrongPassword, hashedPassword)
+	err = CheckPassword(wrongPassword, hashedPassword1)
 	require.EqualError(t, err, bcrypt.ErrMismatchedHashAndPassword.Error())
+
+	hashedPassword2, err := HashPassword(password)
+	require.NoError(t, err)
+	require.NotEmpty(t, hashedPassword2)
+	require.NotEqualf(t, hashedPassword1, hashedPassword2, "two hashed passwords should not be the same")
 }
